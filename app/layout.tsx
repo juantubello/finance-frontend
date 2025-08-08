360a7776a8c015095e115c1aec6b692c379624b7
Juan <juanfernatubello@gmail.com>
2025-08-08 09:45:25 -0300

    changed header color

--- app/layout.tsx
+++ app/layout.tsx
@@ -46,6 +46,8 @@ export default function RootLayout({
         <link rel="apple-touch-icon" href="/icon-192x192.png" />
         <link rel="apple-touch-startup-image" href="/icon-512x512.png" />
         <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#151C2C" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
         <meta name="mobile-web-app-capable" content="yes" />
